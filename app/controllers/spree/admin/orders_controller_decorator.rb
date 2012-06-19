33bb9be07b9553ee6e48d2fc2fa151829509c556
Swapnil <swap.it@hotmail.com>
2012-06-19 17:23:16 +0530

    orders

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -2,6 +2,6 @@ Spree::Admin::OrdersController.class_eval do
   before_filter :order_store, :only => :index
 
   def order_store
-    @order = Spree::Order.by_store(current_store.id) if current_store.id
+    @orders = Spree::Order.by_store(current_store.id) if current_store.id
   end
 end

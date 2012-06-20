b7cdaa4b1f2121a8197a4032dbb772fb72116dcb
Swapnil <swap.it@hotmail.com>
2012-06-20 14:10:34 +0530

    remove filter from plugin

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -1,7 +1,7 @@
-Spree::Admin::OrdersController.class_eval do
-  before_filter :order_store, :only => :index
+#Spree::Admin::OrdersController.class_eval do
+#  before_filter :order_store, :only => :index
 
-  def order_store
-    @orders = Spree::Order.by_store(current_store) if current_store.id
-  end
-end
+#  def order_store
+#    @orders = Spree::Order.by_store(current_store) if current_store.id
+#  end
+#end
